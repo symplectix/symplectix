8b9a2589a6325a78f5bb13768b52045e8ca1aeeb
Kohei Watanabe <mypmc@symplectix.dev>
2025-12-09 00:49:37 +0900

    xf

--- xf/xf_test.rs
+++ xf/xf_test.rs
@@ -1,14 +1,14 @@
 #![allow(missing_docs)]
 
-use xfn::{Adapter, Chain, Reducer};
+use xf::{Adapter, Chain, Reducer};
 
 pub struct Add;
 
-impl xfn::Reducer<i32, i32> for Add {
+impl xf::Reducer<i32, i32> for Add {
     // type Acc = i32;
 
-    fn step(&mut self, acc: i32, v: i32) -> xfn::Step<i32> {
-        xfn::Step::Next(acc + v)
+    fn step(&mut self, acc: i32, v: i32) -> xf::Step<i32> {
+        xf::Step::Next(acc + v)
     }
 
     fn done(&mut self, acc: i32) -> i32 {
@@ -18,12 +18,12 @@ impl xfn::Reducer<i32, i32> for Add {
 
 pub struct VecConj;
 
-impl xfn::Reducer<Vec<i32>, i32> for VecConj {
+impl xf::Reducer<Vec<i32>, i32> for VecConj {
     // type Acc = i32;
 
-    fn step(&mut self, mut acc: Vec<i32>, v: i32) -> xfn::Step<Vec<i32>> {
+    fn step(&mut self, mut acc: Vec<i32>, v: i32) -> xf::Step<Vec<i32>> {
         acc.push(v);
-        xfn::Step::Next(acc)
+        xf::Step::Next(acc)
     }
 
     fn done(&mut self, acc: Vec<i32>) -> Vec<i32> {
@@ -47,14 +47,14 @@ fn mod2(x: i32) -> i32 {
 fn test_map() {
     let conj = VecConj;
 
-    let mut rf = xfn::map(inc).map(mod2).apply(conj);
+    let mut rf = xf::map(inc).map(mod2).apply(conj);
     let mut acc = Vec::with_capacity(10);
     for i in 0..10 {
         match rf.step(acc, i) {
-            xfn::Step::Next(ret) => {
+            xf::Step::Next(ret) => {
                 acc = ret;
             }
-            xfn::Step::Done(ret) => {
+            xf::Step::Done(ret) => {
                 acc = ret;
                 break;
             }
