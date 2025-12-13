6bb2dfe358ea92e38b79855480ee890163d79e34
Kohei Watanabe <mypmc@symplectix.dev>
2025-12-13 17:06:09 +0900

    ano

--- ano/ano_test.rs
+++ ano/ano_test.rs
@@ -1,28 +1,28 @@
 #![allow(missing_docs)]
 
-// use mung::{StepFn, Xform};
+// use ano::{StepFn, Xform};
 
 // struct PushVec;
 
 // impl<T> StepFn<T> for PushVec {
 //     type Acc = Vec<T>;
 
-//     fn step(&mut self, mut acc: Self::Acc, v: T) -> mung::Step<Self::Acc> {
+//     fn step(&mut self, mut acc: Self::Acc, v: T) -> ano::Step<Self::Acc> {
 //         acc.push(v);
-//         mung::Step::Yield(acc)
+//         ano::Step::Yield(acc)
 //     }
 // }
 
 // #[test]
 // fn test_map_filter_step() {
 //     let mut acc = vec![];
-//     let mut sf = mung::map(|x| x * 2 + 1).filter(|x: &i32| 10 < *x && *x < 20).apply(PushVec);
+//     let mut sf = ano::map(|x| x * 2 + 1).filter(|x: &i32| 10 < *x && *x < 20).apply(PushVec);
 //     for i in 0..20 {
 //         match sf.step(acc, i) {
-//             mung::Step::Yield(ret) => {
+//             ano::Step::Yield(ret) => {
 //                 acc = ret;
 //             }
-//             mung::Step::Break(ret) => {
+//             ano::Step::Break(ret) => {
 //                 acc = sf.done(ret);
 //                 break;
 //             }
