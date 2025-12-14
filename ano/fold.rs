25955859573cf5685598bc7f2e47df839d19e8ae
Kohei Watanabe <mypmc@symplectix.dev>
2025-12-14 18:58:18 +0900

    either

--- ano/fold.rs
+++ ano/fold.rs
@@ -50,30 +50,6 @@ pub enum Step<T> {
     Break(T),
 }
 
-impl<T, A, B> Fold<T> for (A, B)
-where
-    A: Fold<T>,
-    B: Fold<T>,
-{
-    type Acc = (<A as Fold<T>>::Acc, <B as Fold<T>>::Acc);
-
-    fn step<Q>(&mut self, acc: Self::Acc, input: &Q) -> Step<Self::Acc>
-    where
-        Q: Borrow<T>,
-    {
-        match (self.0.step(acc.0, input), self.1.step(acc.1, input)) {
-            (Step::Yield(a), Step::Yield(b)) => Step::Yield((a, b)),
-            (Step::Break(a), Step::Yield(b)) => Step::Break((a, b)),
-            (Step::Yield(a), Step::Break(b)) => Step::Break((a, b)),
-            (Step::Break(a), Step::Break(b)) => Step::Break((a, b)),
-        }
-    }
-
-    fn done(self, acc: Self::Acc) -> Self::Acc {
-        (self.0.done(acc.0), self.1.done(acc.1))
-    }
-}
-
 #[derive(Debug)]
 pub struct Map<Sf, F> {
     sf: Sf,
