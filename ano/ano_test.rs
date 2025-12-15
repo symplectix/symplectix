26fd4bfc2dc44cc5eea1c6cbfcae59d03e723aa8
Kohei Watanabe <mypmc@symplectix.dev>
2025-12-15 11:21:44 +0900

    cons conj

--- ano/ano_test.rs
+++ ano/ano_test.rs
@@ -3,21 +3,14 @@
 use std::borrow::{Borrow, ToOwned};
 use std::collections::VecDeque;
 use std::iter::{empty, once};
+use std::marker::PhantomData;
 
 use ano::{Fold, xf};
 
-struct Conj;
+#[derive(Debug)]
+struct Conj<T>(PhantomData<T>);
 
-// struct Conj<T> {
-//     acc: Vec<T>,
-// }
-// impl<T> Conj<T> {
-//     fn new(capacity: usize) -> Self {
-//         Conj { acc: Vec::with_capacity(capacity) }
-//     }
-// }
-
-impl<In> ano::Fold<In, Vec<In::Owned>> for Conj
+impl<In> ano::Fold<In, Vec<In::Owned>> for Conj<In>
 where
     In: ToOwned,
 {
@@ -36,17 +29,14 @@ where
     }
 }
 
-struct Cons;
-// struct Cons<T> {
-//     acc: VecDeque<T>,
-// }
-// impl<T> Cons<T> {
-//     fn new(capacity: usize) -> Self {
-//         Cons { acc: VecDeque::with_capacity(capacity) }
-//     }
-// }
+fn cons<T>() -> Cons<T> {
+    Cons(PhantomData)
+}
 
-impl<In> ano::Fold<In, VecDeque<In::Owned>> for Cons
+#[derive(Debug)]
+struct Cons<T>(PhantomData<T>);
+
+impl<In> ano::Fold<In, VecDeque<In::Owned>> for Cons<In>
 where
     In: ToOwned,
 {
@@ -64,6 +54,10 @@ where
     }
 }
 
+fn conj<T>() -> Conj<T> {
+    Conj(PhantomData)
+}
+
 fn pow2(x: &i32) -> i32 {
     x.pow(2)
 }
@@ -79,62 +73,62 @@ fn even(x: &i32) -> bool {
 #[test]
 fn map() {
     let acc = vec![];
-    let ret = xf::map(pow2).apply(Conj).fold(acc, empty::<i32>());
+    let ret = xf::map(pow2).apply(conj()).fold(acc, empty::<i32>());
     assert_eq!(ret, vec![]);
     let acc = vec![];
-    let ret = xf::map(pow2).apply(Conj).fold(acc, 1..4);
+    let ret = xf::map(pow2).apply(conj()).fold(acc, 1..4);
     assert_eq!(ret, vec![1, 4, 9]);
 }
 
 #[test]
 fn filter() {
     let acc = vec![];
-    let ret = xf::filter(even).apply(Conj).fold(acc, empty::<i32>());
+    let ret = xf::filter(even).apply(conj()).fold(acc, empty::<i32>());
     assert_eq!(ret, vec![]);
     let acc = vec![];
-    let ret = xf::filter(even).apply(Conj).fold(acc, once(1));
+    let ret = xf::filter(even).apply(conj()).fold(acc, once(1));
     assert_eq!(ret, vec![]);
     let acc = vec![];
-    let ret = xf::filter(even).apply(Conj).fold(acc, vec![1, 3, 5]);
+    let ret = xf::filter(even).apply(conj()).fold(acc, vec![1, 3, 5]);
     assert_eq!(ret, vec![]);
     let acc = vec![];
-    let ret = xf::filter(even).apply(Conj).fold(acc, 1..6);
+    let ret = xf::filter(even).apply(conj()).fold(acc, 1..6);
     assert_eq!(ret, vec![2, 4]);
 }
 
 #[test]
 fn take() {
-    let acc = xf::take(0).apply(Conj).fold(vec![], empty::<i32>());
+    let acc = xf::take(0).apply(conj()).fold(vec![], empty::<i32>());
     assert_eq!(acc, vec![]);
-    let acc = xf::take(0).apply(Conj).fold(vec![], 1..);
+    let acc = xf::take(0).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![]);
-    let acc = xf::take(1).apply(Conj).fold(vec![], empty::<i32>());
+    let acc = xf::take(1).apply(conj()).fold(vec![], empty::<i32>());
     assert_eq!(acc, vec![]);
-    let acc = xf::take(0).apply(Conj).fold(vec![], 1..);
+    let acc = xf::take(0).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![]);
-    let acc = xf::take(2).apply(Conj).fold(vec![], 1..3);
+    let acc = xf::take(2).apply(conj()).fold(vec![], 1..3);
     assert_eq!(acc, vec![1, 2]);
-    let acc = xf::take(5).apply(Conj).fold(vec![], 1..3);
+    let acc = xf::take(5).apply(conj()).fold(vec![], 1..3);
     assert_eq!(acc, vec![1, 2]);
-    let acc = xf::take(3).apply(Conj).fold(vec![], 1..);
+    let acc = xf::take(3).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![1, 2, 3]);
 }
 
 #[test]
 fn map_filter_take() {
-    let acc = xf::map(mul3).take(5).filter(even).apply(Conj).fold(vec![], 1..);
+    let acc = xf::map(mul3).take(5).filter(even).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12]);
-    let acc = xf::map(mul3).filter(even).take(5).apply(Conj).fold(vec![], 1..);
+    let acc = xf::map(mul3).filter(even).take(5).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12, 18, 24, 30]);
 
-    let acc = xf::filter(even).map(mul3).take(5).apply(Conj).fold(vec![], 1..);
+    let acc = xf::filter(even).map(mul3).take(5).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12, 18, 24, 30]);
-    let acc = xf::filter(even).take(5).map(mul3).apply(Conj).fold(vec![], 1..);
+    let acc = xf::filter(even).take(5).map(mul3).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12, 18, 24, 30]);
 
-    let acc = xf::take(5).map(mul3).filter(even).apply(Conj).fold(vec![], 1..);
+    let acc = xf::take(5).map(mul3).filter(even).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12]);
-    let acc = xf::take(5).filter(even).map(mul3).apply(Conj).fold(vec![], 1..);
+    let acc = xf::take(5).filter(even).map(mul3).apply(conj()).fold(vec![], 1..);
     assert_eq!(acc, vec![6, 12]);
 }
 
@@ -143,20 +137,14 @@ fn either() {
     // let acc = <Cons<i32> as Fold<i32, VecDeque<_>>>::either::<Conj<i32>>(Cons::new(10), Conj::new(10)).fold(1..5);
     // assert_eq!(acc, (VecDeque::from([4, 3, 2, 1]), vec![1, 2, 3, 4]));
 
-    fn cons() -> impl Fold<i32, VecDeque<i32>, Acc = VecDeque<i32>> {
-        Cons
-    }
-    fn conj() -> impl Fold<i32, Vec<i32>, Acc = Vec<i32>> {
-        Conj
-    }
     let f = cons().either(conj());
     let acc = f.fold((VecDeque::with_capacity(10), Vec::with_capacity(10)), 1..5);
     assert_eq!(acc, (VecDeque::from([4, 3, 2, 1]), vec![1, 2, 3, 4]));
 
     let acc = xf::map(pow2)
         .take(3)
-        .apply(Cons)
-        .either(xf::map(mul3).take(2).apply(Conj))
+        .apply(cons())
+        .either(xf::map(mul3).take(2).apply(conj()))
         .fold((VecDeque::new(), Vec::new()), 1..10);
     assert_eq!(acc, (VecDeque::from([4, 1]), vec![3, 6]));
 }
