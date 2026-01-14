662be992366eb20b39500f390b85e2b59928c30f
Kohei Watanabe <mypmc@symplectix.dev>
2026-01-15 00:30:10 +0900

    just update

---
+++ crates/devtool/src/lib.rs
@@ -0,0 +1,48 @@
+//! This crate `devtool` provides internal tooling.
+
+use std::env;
+use std::path::{
+    Path,
+    PathBuf,
+};
+
+mod format;
+
+trait DevTool {
+    fn run(self, ctx: Context) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Clone)]
+struct Context {
+    #[allow(dead_code)]
+    cargo: PathBuf,
+}
+
+/// Arguments for the devtool cli.
+#[derive(Debug, Clone, clap::Parser)]
+pub struct Cli {
+    #[clap(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, Clone, clap::Subcommand)]
+enum Command {
+    /// Format code.
+    Format(format::Format),
+}
+
+impl Cli {
+    /// Run a tool and wait its result.
+    pub fn run(self) -> anyhow::Result<()> {
+        let project_root = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/../.."));
+        assert!(project_root.join(".git/HEAD").exists());
+        // Always run tools from the project root for consistency.
+        env::set_current_dir(project_root)?;
+
+        let ctx = Context { cargo: PathBuf::from(env!("CARGO")) };
+
+        match self.cmd {
+            Command::Format(t) => t.run(ctx),
+        }
+    }
+}
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::process;
-
-use clap::Parser;
-
-fn main() {
-    let cli = syx::Cli::parse();
-    let status = cli.run().expect("failed to run cli");
-    process::exit(status.code().unwrap_or(1))
-}
