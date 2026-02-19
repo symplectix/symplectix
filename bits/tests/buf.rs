b903ef5705c26f03e500cf71b71d0a6c06e346e9
Kohei Watanabe <mypmc@symplectix.dev>
2026-02-20 03:20:27 +0900

    update

--- bits/tests/buf.rs
+++ bits/tests/buf.rs
@@ -3,7 +3,6 @@ use bits::{
     BitsMut,
     Block,
     Buf,
-    Masking,
 };
 
 #[test]
@@ -95,85 +94,4 @@ fn select1_select0() {
     assert_eq!(b.select1(1), Some(511));
     assert_eq!(b.select0(0), Some(0));
     assert_eq!(b.select0(1), Some(2));
-}
-
-#[test]
-fn masking_and() {
-    let mut a = Buf::<[u64; 4]>::new();
-    a.set1(0);
-    a.set1(1);
-    a.set1(2);
-    a.set1(128);
-    let mut b = Buf::<[u64; 4]>::new();
-    b.set1(1);
-    b.set1(2);
-    b.set1(3);
-    b.set1(192);
-    Masking::and(&mut a, &b);
-    assert_eq!(a.as_ref().unwrap(), &[0b_0110, 0, 0, 0]);
-    let mut c = Buf::<[u64; 4]>::new();
-    Masking::and(&mut b, &c);
-    assert_eq!(b.as_ref(), None);
-    Masking::and(&mut c, &a);
-    assert_eq!(c.as_ref(), None);
-}
-
-#[test]
-fn masking_or() {
-    let mut a = Buf::<[u64; 4]>::new();
-    a.set1(0);
-    a.set1(1);
-    a.set1(2);
-    a.set1(128);
-    let mut b = Buf::<[u64; 4]>::new();
-    b.set1(1);
-    b.set1(2);
-    b.set1(3);
-    b.set1(192);
-    Masking::or(&mut a, &b);
-    assert_eq!(a.as_ref().unwrap(), &[0b_1111, 0, 1, 1]);
-    let mut c = Buf::<[u64; 4]>::new();
-    Masking::or(&mut c, &b);
-    assert_eq!(c.as_ref().unwrap(), &[0b_1110, 0, 0, 1]);
-}
-
-#[test]
-fn masking_not() {
-    let mut a = Buf::<[u64; 4]>::new();
-    a.set1(0);
-    a.set1(1);
-    a.set1(2);
-    a.set1(128);
-
-    let mut b = Buf::<[u64; 4]>::new();
-    b.set1(1);
-    b.set1(2);
-    b.set1(3);
-    b.set1(192);
-
-    Masking::not(&mut a, &b);
-    assert_eq!(a.as_ref().unwrap(), &[0b_0001, 0, 1, 0]);
-
-    let mut c = Buf::<[u64; 4]>::new();
-    Masking::not(&mut c, &a);
-    assert_eq!(c.as_ref(), None);
-}
-
-#[test]
-fn masking_xor() {
-    let mut a = Buf::<[u64; 4]>::new();
-    a.set1(0);
-    a.set1(1);
-    a.set1(2);
-    a.set1(128);
-    let mut b = Buf::<[u64; 4]>::new();
-    b.set1(1);
-    b.set1(2);
-    b.set1(3);
-    b.set1(192);
-    Masking::xor(&mut a, &b);
-    assert_eq!(a.as_ref().unwrap(), &[0b_1001, 0, 1, 1]);
-    let mut c = Buf::<[u64; 4]>::new();
-    Masking::xor(&mut c, &a);
-    assert_eq!(c.as_ref().unwrap(), &[0b_1001, 0, 1, 1]);
 }
---
+++ bits/tests/mask.rs
@@ -0,0 +1,86 @@
+use bits::{
+    BitsMut,
+    Buf,
+    Masking,
+};
+
+#[test]
+fn masking_and() {
+    let mut a = Buf::<[u64; 4]>::new();
+    a.set1(0);
+    a.set1(1);
+    a.set1(2);
+    a.set1(128);
+    let mut b = Buf::<[u64; 4]>::new();
+    b.set1(1);
+    b.set1(2);
+    b.set1(3);
+    b.set1(192);
+    Masking::and(&mut a, &b);
+    assert_eq!(a.as_ref().unwrap(), &[0b_0110, 0, 0, 0]);
+    let mut c = Buf::<[u64; 4]>::new();
+    Masking::and(&mut b, &c);
+    assert_eq!(b.as_ref(), None);
+    Masking::and(&mut c, &a);
+    assert_eq!(c.as_ref(), None);
+}
+
+#[test]
+fn masking_or() {
+    let mut a = Buf::<[u64; 4]>::new();
+    a.set1(0);
+    a.set1(1);
+    a.set1(2);
+    a.set1(128);
+    let mut b = Buf::<[u64; 4]>::new();
+    b.set1(1);
+    b.set1(2);
+    b.set1(3);
+    b.set1(192);
+    Masking::or(&mut a, &b);
+    assert_eq!(a.as_ref().unwrap(), &[0b_1111, 0, 1, 1]);
+    let mut c = Buf::<[u64; 4]>::new();
+    Masking::or(&mut c, &b);
+    assert_eq!(c.as_ref().unwrap(), &[0b_1110, 0, 0, 1]);
+}
+
+#[test]
+fn masking_not() {
+    let mut a = Buf::<[u64; 4]>::new();
+    a.set1(0);
+    a.set1(1);
+    a.set1(2);
+    a.set1(128);
+
+    let mut b = Buf::<[u64; 4]>::new();
+    b.set1(1);
+    b.set1(2);
+    b.set1(3);
+    b.set1(192);
+
+    Masking::not(&mut a, &b);
+    assert_eq!(a.as_ref().unwrap(), &[0b_0001, 0, 1, 0]);
+
+    let mut c = Buf::<[u64; 4]>::new();
+    Masking::not(&mut c, &a);
+    assert_eq!(c.as_ref(), None);
+}
+
+#[test]
+fn masking_xor() {
+    let mut a = Buf::<[u64; 4]>::new();
+    a.set1(0);
+    a.set1(1);
+    a.set1(2);
+    a.set1(128);
+    let mut b = Buf::<[u64; 4]>::new();
+    b.set1(1);
+    b.set1(2);
+    b.set1(3);
+    b.set1(192);
+    Masking::xor(&mut a, &b);
+    assert_eq!(a.as_ref().unwrap(), &[0b_1001, 0, 1, 1]);
+    let mut c = Buf::<[u64; 4]>::new();
+    Masking::xor(&mut c, &a);
+    assert_eq!(c.as_ref().unwrap(), &[0b_1001, 0, 1, 1]);
+}
