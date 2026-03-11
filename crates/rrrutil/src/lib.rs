ab3c7db8de889c0ad8ecdd4e896ebf928c899bda
Kohei Watanabe <mypmc@symplectix.dev>
2026-03-11 15:10:18 +0900

    rrrutil

--- crates/rrrutil/src/lib.rs
+++ crates/rrrutil/src/lib.rs
@@ -1,4 +1,4 @@
-//! Build time utilities for rrr.
+//! Provides helper utilities for rrr implementations.
 
 use std::io::Write;
 use std::path::Path;
@@ -25,7 +25,8 @@ fn comb_table(size: usize) -> Vec<Vec<u64>> {
     table
 }
 
-/// Writes static values and functions to OUT_DIR.
+/// Build time utilities for rrr, which writes static values
+/// and functions to OUT_DIR.
 ///
 /// It is a good idea to choose `b + 1` as a power of two,
 /// so that the bits for `class` can be fully used (bitpacking).
@@ -65,14 +66,63 @@ static COMB: [[{item_type}; {table_len}]; {table_len}] = {table:?};
     })
 }
 
+// NOTE: COMB[n][k] is the number of ways to choose k items
+// from n items without repetition and without order.
+
+/// Encode using a static table.
+#[macro_export]
+macro_rules! encode {
+    ($data:expr) => {{
+        let data = $data & ((1 << SIZE) - 1);
+
+        let class = data.count_ones();
+        let offset = {
+            let mut b = SIZE;
+            let mut c = class as usize;
+            let mut o = 0;
+
+            while b > 0 && c > 0 {
+                b -= 1;
+                if data & (1 << b) != 0 {
+                    o += COMB[b][c];
+                    c -= 1;
+                }
+            }
+            o
+        };
+        (class as u8, offset)
+    }};
+}
+
+/// Decode using a static table.
+#[macro_export]
+macro_rules! decode {
+    ($class:expr, $offset:expr) => {{
+        let mut data = 0;
+        let mut b = SIZE;
+        let mut c = $class as usize;
+        let mut o = $offset;
+
+        while b > 0 && c > 0 {
+            b -= 1;
+            if o >= COMB[b][c] {
+                data |= 1 << b;
+                o -= COMB[b][c];
+                c -= 1;
+            }
+        }
+        data
+    }};
+}
+
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
-    use runfiles::{
-        Runfiles,
-        rlocation,
+    use std::path::Path;
+    use std::{
+        env,
+        fs,
     };
+
     use serde::{
         Deserialize,
         Serialize,
@@ -89,8 +139,8 @@ mod tests {
     // Verifies that the generated table is equivalent to another table produced by
     // `comb.py`.
     fn eq_to_comb_py(n: usize) {
-        let r = Runfiles::create().expect("failed to create Runfiles");
-        let path = rlocation!(r, format!("_main/rrr/comb_table_{n}.json")).unwrap();
+        let testdata = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/testdata"));
+        let path = testdata.join(format!("comb_table_{n}.json"));
         let json_file =
             fs::OpenOptions::new().read(true).open(path).expect("failed to open a json file");
         let buf = std::io::BufReader::new(json_file);
